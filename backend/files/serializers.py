5947519403b4465e6d655cf2a375baa2f1e14b86
Vijay Subramanya <vijay.subramanya@gmail.com>
2025-06-25 20:54:57 +0530

    delete unused folder

--- backend/files/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import File
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ['id', 'file', 'original_filename', 'file_type', 'size', 'uploaded_at']
-        read_only_fields = ['id', 'uploaded_at'] 
